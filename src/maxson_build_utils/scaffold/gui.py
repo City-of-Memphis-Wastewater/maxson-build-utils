745656ca12d90038d7aba64d5d685955353667a7
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 13:22:07 -0500

    scaffold-gui.py moved to scaffold/gui.py, logger added

--- src/maxson_build_utils/scaffold/gui.py
+++ src/maxson_build_utils/scaffold/gui.py
@@ -1,9 +1,11 @@
 # src/maxson_build_utils/scaffold_gui.py
 from __future__ import annotations
 from pathlib import Path
+import logging
+logger = logging.getLogger(__name__)
 
-from .helpers import write_str_to_file
-from .pyproject import PyProject
+from ..helpers import write_str_to_file
+from ..pyproject import PyProject
 
 def run_init_gui(root_dir: Path | str | None = None) -> None:
     pyproject = PyProject(root_dir)
