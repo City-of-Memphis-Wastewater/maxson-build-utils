01f0d33d8cc4f8c0fcc4fd104f8f8d0333c967f2
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 12:31:00 -0500

    change scaffold_context to reflect gains in how PyProject is leveraged for names, rather than names.py

--- src/maxson_build_utils/scaffold_context.py
+++ src/maxson_build_utils/scaffold_context.py
@@ -10,15 +10,16 @@ from __future__ import annotations
 from pathlib import Path
 from maxson_build_utils import PyProject 
 from dworshak_config import DworshakConfig
-from maxson_build_utils.names import get_src_dir, get_pretty_name, get_app_name
 
-pyproject=PyProject()
+_pyproject = PyProject()
 
-APP_NAME = get_app_name()
+APP_NAME = _pyproject.app_name
+APP_NAME_PRETTY = _pyproject.pretty_name
+IMPORT_NAME = _pyproject.import_name
+SRC_DIR = _pyproject.src_dir
+SRC_FOLDER_NAME = IMPORT_NAME
 SERVICE = APP_NAME
 DESCRIPTION_STR = pyproject.get("project","description")
-SRC_FOLDER_NAME = get_src_dir()
-APP_NAME_PRETTY = get_pretty_name()
 APP_DIR = Path.home() / f".{APP_NAME}"
 APP_DIR.mkdir(parents=True,exist_ok=True)
 LOG_FILE_PATH = APP_DIR / f"{APP_NAME}_errors.log"
