71daa477b162cacf1e3319f7e4b8cddcf9507c4e
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 12:09:58 -0500

    correct local context.py to leverage pyproject.toml

--- src/maxson_build_utils/context.py
+++ src/maxson_build_utils/context.py
@@ -1,11 +1,16 @@
 # src/maxson_build_utils/context.py
 from __future__ import annotations
 from pathlib import Path
-from maxson_build_utils.names import get_src_dir, get_pretty_name, get_app_name, get_src_name
 
-APP_NAME = get_app_name()
-SRC_FOLDER_NAME = get_src_name()
-APP_NAME_PRETTY = get_pretty_name()
+from .pyproject import PyProject
+
+_pyproject = PyProject()
+
+APP_NAME = _pyproject.app_name
+APP_NAME_PRETTY = _pyproject.pretty_name
+IMPORT_NAME = _pyproject.import_name
+SRC_DIR = _pyproject.src_dir
+SRC_FOLDER_NAME = IMPORT_NAME
 APP_DIR = Path.home() / f".{APP_NAME}"
 APP_DIR.mkdir(parents=True,exist_ok=True)
 LOG_FILE_PATH = APP_DIR / f"{APP_NAME}_errors.log"
