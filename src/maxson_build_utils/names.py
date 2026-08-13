0e5984bc2a9ee14538d8dc53849d959c0f7a60c0
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-08-13 02:46:16 -0500

    type hint correction

--- src/maxson_build_utils/names.py
+++ src/maxson_build_utils/names.py
@@ -20,7 +20,7 @@ def to_title_case(value: str) -> str:
     words = re.split(r"[-_\s]+", value.strip())
     return " ".join(word.capitalize() for word in words if word)
 
-def get_src_name(path:str|Path|None=None) -> Path:
+def get_src_name(path:str|Path|None=None) -> str:
     from .pyproject import PyProject
     keys=["tool","maxson-build-utils","names","import"]
     pyproject=PyProject()
