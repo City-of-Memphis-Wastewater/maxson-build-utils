d75089d1683e049f7b493a34d87b870814f5611c
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 13:02:09 -0500

    allow scaffold files to have a root dir fed

--- src/maxson_build_utils/scaffold_gui.py
+++ src/maxson_build_utils/scaffold_gui.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 from .helpers import write_str_to_file
 from .pyproject import PyProject
 
-pyproject = PyProject()
-def run_init_gui( ):
+def run_init_gui(root_dir: Path | str | None = None) -> None:
+    pyproject = PyProject(root_dir)
     write_str_to_file(pyproject.src_dir / "gui.py", text = raw_gui_str)
 
 raw_gui_str = '''
