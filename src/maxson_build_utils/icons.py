a564923c7cce9150cdec29c6bc406c2beb11e9b6
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-13 12:20:46 -0500

    ensure copy icons has what it needs

--- src/maxson_build_utils/icons.py
+++ src/maxson_build_utils/icons.py
@@ -3,27 +3,24 @@ import shutil
 from pathlib import Path
 from importlib.resources import files
 
-from .names import get_src_dir
-
 from .pyproject import PyProject
-
-
-def get_icons_dir(path: Path | str | None = None) -> Path:
-    pyproject = PyProject(path)
-    return pyproject.src_dir / "data" / "icons"
 
 def bundled_icons():
     return files("maxson_build_utils") / "data" / "icons"
 
-def copy_stock_icons(path: Path | str | None = None) -> Path:
-    if path is None:
-        path = PyProject().
+def copy_stock_icons(
+    dst: Path | str | None = None
+    pyproject_path: Path | str | None = None
+    ) -> Path:
+    if dst is None:
+        pyproject = PyProject(pyproject_path) # force local pyproject.toml
+        path pyproject.icons_dir
 
-    path = Path(path)
+    dst = Path(dst)
     path.mkdir(parents=True, exist_ok=True)
 
     for icon in bundled_icons().iterdir():
         if icon.is_file():
-            shutil.copy2(icon, path / icon.name)
+            shutil.copy2(icon, dst / icon.name)
 
-    return path
+    return dst
