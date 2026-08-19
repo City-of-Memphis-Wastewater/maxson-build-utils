64cdeff00a15aa11a59aa3feca3c41e03d1933b6
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-08-19 12:39:06 -0500

    helpers, core, config, gitignore, git, readme, all added to scaffold. uv not

--- src/maxson_build_utils/scaffold/__init__.py
+++ src/maxson_build_utils/scaffold/__init__.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 from .changelog import run_init_changelog
 from .cli import run_init_cli
 from .context import run_init_context
+from .config import run_init_config
+from .core import run_init_core
+from .helpers import run_init_helpers
 from .flatpak import run_init_flatpak
 from .gui import run_init_gui
 from .icons import run_init_icons
@@ -12,17 +15,24 @@ from .pyproject import run_init_pyproject
 from ._init import run_init_init
 from ._main import run_init_main
 from .logging_setup import run_init_logging_setup
+from .uv import run_uv_init_custom
 
 __all__ = [
+    # --- source code --
+    "run_init_pyproject",
+    "run_init_readme",
+    "run_init_gitignore",
+    "run_init_git",
     "run_init_changelog",
+    "run_init_src",
     "run_init_cli",
     "run_init_context",
-    "run_init_flatpak",
+    "run_init_config",
     "run_init_gui",
     "run_init_icons",
-    "run_init_src",
-    "run_init_pyproject",
     "run_init_init",
     "run_init_main",
     "run_init_logging_setup",
+    # --- packaging ---
+    "run_init_flatpak",
 ]
---
+++ src/maxson_build_utils/scaffold/uv.py
@@ -0,0 +1,12 @@
+#
+"""
+Dont include .python-version file
+Use my custom .gitignore standard file
+Setup git.
+uv init doesn't actually setup uv venv, so what does it even mean?
+Don't use the standard main.py file in root.
+My pyproject standard replaces the uv init one - can mine work without first seeing the uv init generated pyproject.toml?
+Readme.md should be generated to include the description (which may or may not exist yet but might be the only thing prompted for), and shouldninclude the helotree reference section.
+"""
+def run_uv_init_custom():
+    pass
