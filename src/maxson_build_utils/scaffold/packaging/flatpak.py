799744cbab8484629df743a0d6e6a57a05eda69b
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-09-14 15:53:05 -0500

    add app categories to flatpak xml

--- src/maxson_build_utils/scaffold/packaging/flatpak.py
+++ src/maxson_build_utils/scaffold/packaging/flatpak.py
@@ -44,9 +44,7 @@ modules:
     sources:
       - type: dir
         path: ../..
-      - type: dir
-        path: ../../build/vendor-wheels
-        dest: build/vendor-wheels
+ 
 """
 
 DESKTOP_TEMPLATE = """[Desktop Entry]
@@ -70,6 +68,11 @@ METAINFO_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
     </p>
   </description>
   <launchable type="desktop-id">{APP_ID}.desktop</launchable>
+  <icon type="stock">io.github.city_of_memphis_wastewater.maxson-build-utils</icon>
+  <categories>
+    <category>Utility</category>
+    <category>Development</category>
+  </categories>
 </component>
 """
 
