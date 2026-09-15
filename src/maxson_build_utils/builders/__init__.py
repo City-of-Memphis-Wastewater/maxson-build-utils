de2e3675e794b3c32ab546907f2fcae24ef0759c
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-09-14 21:11:20 -0500

    expose build_msix in builders/__init__.py

--- src/maxson_build_utils/builders/__init__.py
+++ src/maxson_build_utils/builders/__init__.py
@@ -2,6 +2,7 @@ from .deb import build_debian_package
 from .flatpak import build_flatpak
 from .linux_app_image import build_linux_appimage
 from .macos_dmg import build_macos_dmg
+from .msix import build_msix
 from .pyinstaller import run_build_executable
 from .shiv import run_build_pyz
 from .validate import TargetBuild, validate_build_target
@@ -11,6 +12,7 @@ __all__ = [
     "build_flatpak",
     "build_linux_appimage",
     "build_macos_dmg",
+    "build_msix",
     "run_build_executable",
     "run_build_pyz",
     "validate_build_target",
