63186e4ff580cf39326163c06444f4c1c77553ce
George Clayton Bennett <george.bennett@memphistn.gov>
2026-09-23 14:16:11 -0500

    campaign, ./vendor_for_buildozer/ target changed to ./vendor/packages/

--- packaging/buildozer/kivy-build-buildozer.sh
+++ packaging/buildozer/kivy-build-buildozer.sh
@@ -16,7 +16,7 @@ set -euo pipefail
 #  suds-py3 \
 #  tzdata \
 #  urllib3 \
-#  --target ./vendor_for_buildozer
+#  --target ./vendor/packages/
 
 
 # Run from packaging/kivy directory
--- src/maxson_build_utils/scaffold/packaging/buildozer.py
+++ src/maxson_build_utils/scaffold/packaging/buildozer.py
@@ -34,8 +34,7 @@ source.dir = ../../
 source.include_exts = py,png,jpg,kv,atlas,whl,wsdl,json
 
 # (list) Source files to include using pattern matching
-source.include_patterns = src/*,./build/vendor_for_buildozer/*
-#source.include_patterns = ./src/*,./build/vendor-wheels/
+source.include_patterns = src/*,./vendor/packages/*
 
 # (list) Source directories to exclude
 source.exclude_dirs = tests,bin,.git,.venv,.pytest_cache,docs,logs,packaging
