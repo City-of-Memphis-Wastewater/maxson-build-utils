1df479ea8280bcab503b6b6191967736f8474f1b
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-09-24 00:53:41 -0500

    scaffolded gui should be called tk-gui

--- src/maxson_build_utils/scaffold/source/cli.py
+++ src/maxson_build_utils/scaffold/source/cli.py
@@ -117,7 +117,7 @@ add_typer_helptree(
     hidden=False,
 )
 
-@app.command(name="gui")
+@app.command(name="tk_gui")
 def gui_command(
     auto_close: int = typer.Option(0,
    "--auto-close", "-c",
