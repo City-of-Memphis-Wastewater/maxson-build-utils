0fc4d110cf42c2a81972f19ee85c66b311fa62e5
George Bennett (bergamot-MacOS) <george.bennett@memphistn.gov>
2026-09-23 23:29:23 -0500

    add overwrite arg to run_init_version

--- src/maxson_build_utils/scaffold/source/_version.py
+++ src/maxson_build_utils/scaffold/source/_version.py
@@ -7,10 +7,7 @@ logger = logging.getLogger(__name__)
 from ...helpers import write_str_to_file, WriteResult
 from ...pyproject import MaxsonPyProject
 
-def run_init_version(root_dir: Path | str | None = None) -> WriteResult:
-    pyproject = MaxsonPyProject(root_dir)
-
-    raw_version_str='''
+raw_version_str='''
 # src/__IMPORT_NAME__/_version.py
 from pathlib import Path
 from .context import APP_NAME
@@ -34,14 +31,25 @@ def get_version() -> str:
 
 __version__ = get_version()
 '''
+
+raw_version_num_str='''0.1.0
+
+'''
+
+def run_init_version(
+    root_dir: Path | str | None = None,
+    overwrite: bool = False,
+) -> WriteResult:
+    pyproject = MaxsonPyProject(root_dir)
+
     raw_version_str=raw_version_str.replace("__IMPORT_NAME__", pyproject.import_name)
     return write_str_to_file(pyproject.src_dir / "_version.py", text = raw_version_str)
 
-def run_init_version_num(root_dir: Path | str | None = None) -> WriteResult:
+def run_init_version_num(
+    root_dir: Path | str | None = None
+    overwrite: bool = False,
+) -> WriteResult:
     pyproject = MaxsonPyProject(root_dir)
 
-    raw_version_num_str='''0.1.0
-
-'''
     raw_version_num_str=raw_version_num_str.replace("__IMPORT_NAME__", pyproject.import_name)
     return write_str_to_file(pyproject.src_dir / "VERSION", text = raw_version_num_str)
